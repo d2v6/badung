9535de55364d3af5f8f0c3ec2879959162028cb1
yonatan-nyo <yonatan.njoto@gmail.com>
2025-11-12 00:01:15 +0700

    created hoempage ui

--- badung/scripts/main_menu.gd
+++ badung/scripts/main_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
 
-func _on_start_pressed() -> void:
+func _on_tutorial_pressed() -> void:
+	SceneTransition.change_scene("res://scene/stages/tutorial.tscn")
+
+
+func _on_start_button_pressed() -> void:
 	SceneTransition.change_scene("res://scene/stages/stage1.tscn")
 
 
-func _on_tutorial_pressed() -> void:
-	SceneTransition.change_scene("res://scene/stages/tutorial.tscn")
+func _on_quit_pressed() -> void:
+	get_tree().quit()
